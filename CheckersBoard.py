f0d6adbc233bb7d3342e3228273e8d6e9e9e1166
houxinha <xinhao.hou@mail.utoronto.ca>
2019-10-09 19:52:56 -0400

    adding blank methods

--- CheckersBoard.py
+++ CheckersBoard.py
@@ -1,1 +1,26 @@
-pass
+class Checkers:
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def otherplayer(self, player):
+        pass
+
+    def get(self):
+        pass
+
+    def validCoordinate(self, row, col):
+        pass
+
+    def jump(self, row, col, drow, dcol):
+        pass
+
+    def move(self, row, col):
+        pass
+
+    def getCount(self):
+        pass
+
