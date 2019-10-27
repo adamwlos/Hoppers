c5b05134d1fb324c7b6481371c9f0d794146974f
wloszcza <adam.wloszczak@mail.utoronto.ca>
2019-10-27 19:34:50 -0400

    Add Player classes and easy Computer difficulty
    Implement Player class as a parent class to create consistency between
    PlayerHuman and PlayerKing. Same goes for the Computer's counterpart.
    
    PlayerComputer and PlayerComputerKing both has an easy and medium
    difficulty level. They compute a move based on there respected
    search algorithms. The Easy mode uses a random search and returns a
    random possible move. The Medium mode has not yet been implemented but
    will search based on finding a large amount of pieces to jump over.
    
    Unlike the computer player's, PlayerHuman and PlayerKing get their
    moves from the user input through the pygame visualizer. Thus, the
    get_moves() method should only transform the coordinates into a Move
    object.

---
+++ Player.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+import Move
+import Checkers
+
+class Player(ABC):
+    """ Player serves as parent class to PlayerHuman, PlayerKing, PlayerComputer, and PlayerKingComputer
+        This class is not for in game use but rather an abstract class that enforces consistency and 
+        for a base that can allow for player types to become interchangable. Player inherits ABC as
+        this is pythons way of signifying the class is abstract.
+    """
+
+    def __init__(self, player: str, checkers: Checkers):
+        self._player = player
+        self._checkers = checkers
+    
+    #@abstractmethod
+    def get_move(self) -> Move:
+        """ Retrieves a valid move for the player to make
+        """
+        return
