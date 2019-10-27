bd0de277b6ca4e64baa8b2edb7a0ef12d1851224
Ronald Chen <ronixchen@yahoo.com>
2019-10-27 19:30:46 -0400

    Make visualizer.py into a class

--- visualizer.py
+++ visualizer.py
@@ -5,45 +5,53 @@ BLACK = (0,0,0)
 GREY = (100,100,100)
 WHITE = (255,255,255)
 DIMENSION = 8# Dimension of game board
-SIZE = 50 # Dimension of a grid tile
+SIZE = 50 # dimension of a grid tile
 
-# proof of concept
-def getInput():
-    keypressed = pygame.event.get()
-    return keypressed
+class Visualizer:
 
-"""
-Create a DIMENSION*DIMENSION grid with alternating white and grey tiles, with
-tiles of dimension SIZE*SIZE
-By Ronald, on 10/27/2019
-"""
-def create_initial_grid(self):
-    background = pygame.Surface(WINDOW)
-    for row in range(DIMENSION):
-        for column in range(DIMENSION):
-            rect = pygame.Rect(column*SIZE,row*SIZE,SIZE,SIZE)
-            """alternate white and grey tiles every column,
-            switch order every row"""
-            if(((column%2)+(row%2))%2 == 0):
-                pygame.draw.rect(background, WHITE, rect)
-            else:
-                pygame.draw.rect(background, GREY, rect)
-    screen.blit(background, (0,0))
-    pygame.display.update()
-    return
+    screen: pygame.Surface
+    
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(WINDOW, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        
+    def getInput(self):
+        keypressed = pygame.event.get()
+        return keypressed
+
+    """
+    Create a DIMENSION*DIMENSION grid onto the given screen with alternating
+    white and grey tiles, with tiles of dimension SIZE*SIZE
+    By Ronald, on 10/27/2019
+    """
+    def create_initial_grid(self) -> None:
+        background = pygame.Surface(WINDOW)
+        for row in range(DIMENSION):
+            for column in range(DIMENSION):
+                rect = pygame.Rect(column*SIZE,row*SIZE,SIZE,SIZE)
+                """alternate white and grey tiles every column,
+                switch order every row"""
+                if(((column%2)+(row%2))%2 == 0):
+                    pygame.draw.rect(background, WHITE, rect)
+                else:
+                    pygame.draw.rect(background, GREY, rect)
+        self.screen.blit(background, (0,0))
+        pygame.display.update()
+
+    def quit(self):
+        pygame.quit()
 
 if __name__ == "__main__":
-    pygame.init()
-    is_running = True
     
-    screen = pygame.display.set_mode(WINDOW, pygame.HWSURFACE | pygame.DOUBLEBUF)
-    create_initial_grid()
+    visualizer = Visualizer()
+    visualizer.create_initial_grid()
+    
+    is_running = True
     while(is_running):
-        
-        a = getInput()
-        if a:
-            print(a)
-        for event in a:
+        events = visualizer.getInput()
+        if events:
+            print(events)
+        for event in events:
             if event.type == pygame.QUIT:
-                pygame.quit()
+                visualizer.quit()
                 is_running = False
