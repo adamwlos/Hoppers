3e5ea76bea6ce0b9bc2b867b8530b3d81e030bdc
houxinha <xinhao.hou@mail.utoronto.ca>
2019-10-26 21:53:35 -0400

    proof of concept

--- visualizer.py
+++ visualizer.py
@@ -1,6 +1,16 @@
 import pygame
 
-
+# proof of concept
 def getInput():
-    keypressed = pygame.key.get_pressed()
+    keypressed = pygame.event.get()
+
+    return keypressed
+
+if __name__ == "__main__":
+    screen = pygame.display.set_mode((500, 500), pygame.HWSURFACE | pygame.DOUBLEBUF)
+
+    while(True):
+        a = getInput()
+        if a:
+            print(a)
 
