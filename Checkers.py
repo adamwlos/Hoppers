450829329e7c6b344a9353c4a2376a4d7277a0d6
sunnywu89 <yuchieh.wu@mail.utoronto.ca>
2019-10-27 18:23:30 -0400

    CheckersController InProgress. Yu-Chieh

---
+++ Checkers.py
@@ -0,0 +1,23 @@
+from CheckersBoard import CheckersBoard
+
+
+class Checkers:
+    board: CheckersBoard
+
+    def __init__(self, board):
+        pass
+
+    def is_gameover(self):
+        pass
+
+    def get_players_turn(self):
+        pass
+
+    def get_winner(self):
+        pass
+
+    def get_count(self, player):
+        return None
+
+    def get_board_string(self):
+        pass
