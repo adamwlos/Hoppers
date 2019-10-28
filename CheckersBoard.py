048d7da7ce07b9dd65a33c170f9652f902c79ce0
Zuhair Syed <zu.syed@mail.utoronto.ca>
2019-10-27 20:31:05 -0400

    Complete CheckersBoard __init__
    Complete __init__ function in CheckersBoard class. This function
    contains the dimensions of the board, types of pieces on the board and
    the representation of the board. The board is also initiated, and all
    the player pieces are added to their appropriate starting positions.
    The board is represented in a 2D list.

--- CheckersBoard.py
+++ CheckersBoard.py
@@ -1,7 +1,48 @@
 class Checkers:
 
     def __init__(self):
-        pass
+        """
+        Contains properties of CheckersBoard including dimensions, player
+        piece types, and board
+        
+        """
+        self.dimension = 10
+        self.player_1 = "X"
+        self.player_2 = "O"
+        self.player_1_king = "KX"
+        self.player_2_king = "KO"
+        self.empty = " "
+        self.turn = self.player_1 
+        self.board = [[],[],[],[],[],[],[],[],[],[]]
+        # initiate the game board with all of player_1 and player_2 pieces
+        # add player 1 pieces to player 1 side of the board            
+        for i in range(3):
+             for j in range(9):
+                # add player 1 peices to each row i on alternating spaces
+                if(i%2 == 0 and j%2 != 0):
+                    self.board[i].append(self.player_1)
+                elif(i%2 == 0 and j%2 == 0):
+                    self.board[i].append(self.empty)
+
+                if(i%2 != 0 and j%2 == 0):
+                    self.board[i].append(self.player_1)
+                elif(i%2 != 0 and j%2 != 0):
+                    self.board[i].append(self.empty)
+
+        # add player 2 pieces to player 2 side of the board                     
+        for k in range(6,9):
+            for p in range(9):
+                # add player 2 peices to each row k on alternating spaces
+                if(k%2 == 0 and p%2 != 0):
+                    self.board[k].append(self.player_2)
+                elif(k%2 == 0 and p%2 == 0):
+                    self.board[k].append(self.empty)
+
+                if(k%2 != 0 and p%2 == 0):
+                    self.board[k].append(self.player_2)
+                elif(k%2 != 0 and p%2 != 0):
+                    self.board[k].append(self.empty)
+                    
 
     def __str__(self):
         pass
