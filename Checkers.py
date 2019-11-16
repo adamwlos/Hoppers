1a0ce60bbdacf22065ce572f9dada3cae25ce057
sunnywu89 <yuchieh.wu@mail.utoronto.ca>
2019-11-15 21:36:54 -0500

    Yu-Chieh update checkers.py

--- Checkers.py
+++ Checkers.py
@@ -2,22 +2,41 @@ from CheckersBoard import CheckersBoard
 
 
 class Checkers:
-    board: CheckersBoard
+    board = CheckersBoard
+    whos_turn = CheckersBoard.player_1
+    dimension = 10
 
-    def __init__(self, board):
+    def __init__(self):
+        pass
+
+    def get_whos_turn(self):
+        return self.whos_turn
+
+    def jump(self):
+        pass
+
+    def has_move(self):
         pass
 
     def is_gameover(self):
         pass
 
-    def get_players_turn(self):
-        pass
-
     def get_winner(self):
-        pass
+        if self.is_gameover():
+            p1_token = self.get_count(self.board.player_1)
+            p2_token = self.get_count(self.board.player_2)
+            if p1_token > p2_token:
+                return self.board.player_1
+            elif  p2_token > p1_token:
+                return self.board.player_2
+            else:
+                return self.board.empty
+        else:
+            return self.board.empty
+            return
 
     def get_count(self, player):
-        return None
+        return self.board.get_count(player)
 
     def get_board_string(self):
-        pass
+        return str(self.board)
