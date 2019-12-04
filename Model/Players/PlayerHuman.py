24b89b711ecb060b2be069d66f92e5466aeeefee
wloszcza <adam.wloszczak@mail.utoronto.ca>
2019-12-03 23:35:22 -0500

    Change final_row/col to direction in base
    Changed final_row/col to direction of move to be consistent with the
    rest of the classes.

--- Model/Players/PlayerHuman.py
+++ Model/Players/PlayerHuman.py
@@ -13,10 +13,10 @@ class PlayerHuman(Player):
         self._player = player
         self._checkers = checkers
 
-    def get_move(self, row: int, col: int, final_row: int, final_col: int):
+    def get_move(self, row: int, col: int, drow: int, dcol: int):
         """ Pygame will retrieve the coordinates of the tile and pass those 
         into getMove to return a Move object.   
         """
         # Row, col are the coordinates of the piece you are moving onto some 
         # tile: final_row, final_col
-        return Move(row, col, final_row, final_col)
+        return Move(row, col, drow, dcol)
